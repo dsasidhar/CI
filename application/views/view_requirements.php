55ed8d22af8cf5398db9433019a2dad3928bf6f5
Avinash Vundyala <vundyala.avinash@gmail.com>
2014-10-01 22:01:23 +0530

    Edit Requirements

--- application/views/view_requirements.php
+++ application/views/view_requirements.php
@@ -190,10 +190,45 @@
 							</div>
 							<div class="inbox-body" id="edit-content" style="min-height:400px;">
 								<div class="alert alert-info" role="alert" style=" maring:auto; margin-top:100px; width:50%;">
-									Select to Manage
+									Select to Manage Requirements
 								</div>
 							</div>
-						</div>						
+						</div>
+						<div id="Edit Requirements">
+							<div class="inbox-head">
+								<h3>Edit Requirements</h3>
+							</div>
+							<div class="inbox-body" id="edit-content" style="min-height:400px;">
+								<div class="form-group">
+									<div class="form-group">
+										<input type="text" class="form-control" name="requirementname" placeholder="Requirement Name">
+									</div>
+								</div>
+								<div class="row">
+									<div class="form-group col-md-6">
+										<select name="type" class="form-control"  id="type">
+											<option value="default">Select Type</option>
+											<option value="functional">Functional</option>
+											<option value="nonfunctional">Non-Functional</option>
+										</select>
+									</div>
+									<div class="form-group col-md-6">
+										<select name="priority" class="form-control" id="priority">
+											<option value="default">Select Priority</option>
+											<option value="1">Very High</option>
+											<option value="2">High</option>
+											<option value="3">Medium</option>
+											<option value="4">Low</option>
+											<option value="5">Very Low</option>
+										</select>
+									</div>
+								</div>
+								<div class="form-group">
+									<textarea class="form-control" rows="4" name="requirementdescription" placeholder="Requirement Description"></textarea>
+								</div>
+							</div>
+						</div>	
+
 					</aside>
 				</div>
 			</section>
