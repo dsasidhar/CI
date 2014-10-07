a92c98bff5cdf5ca065f32b592af4041b87c6509
Avinash Vundyala <vundyala.avinash@gmail.com>
2014-10-07 06:32:38 +0530

    Bug report

--- application/views/view_testcase.php
+++ application/views/view_testcase.php
@@ -23,135 +23,43 @@
 						<div class="user-head">
 							<h3>Projects</h3>
 						</div>
-						<div>
-							<div class="modal fade" id="addReqGroup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div>						
+							<div class="modal fade" id="reportBug" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 								<div class="modal-dialog">
 									<div class="modal-content">
 										<div class="modal-header">
 											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-											<h4 class="modal-title">Add TestCase</h4>
+											<h4 class="modal-title">Report Bug</h4>
 										</div>
 										<div class="modal-body">
-											<form role="form" id="group">
-												<div class="form-group">
-													<select name="parent" class="form-control"  id="parent_project_id" disabled>
-														<option value="default">Select Project</option>
-														<?php foreach($project_requirements as $project){ ?>
-														<option value="<?= $project['project_id']?>"><?= $project['project_name']?></option>
-														<?php } ?>
-													</select>
+											<form class="form-horizontal" role="form">
+												<div class="form-group row">
+													<div class="col-md-6">
+														<input class="form-control" type="text" name="bugname" placeholder="Bug Name">
+													</div>
+													<div class="col-md-6">
+														<textarea class="form-control" name="bugdescription" placeholder="Bug Description"></textarea>
+													</div>
 												</div>
+												<div class="form-group row">
+													<div class="col-md-6">
+														<select  class="form-control" name="bugstatus">
+															<option value="default">Select Status</option>
+														</select>
+													</div>
+												</div>
+												<br><br>
 												<div class="form-group">
-													<input name="parentname" class="form-control" type="text"  id="parentname" disabled/>
-													<input name="parent" type="hidden" id="parentid"/>
-												</div> 
-												<div class="form-group">
-													<input type="text" class="form-control" name="requirementname" id="req_group_name" placeholder="Requirement Group Name">
-												</div>												                      
-												<button class="btn btn-lg btn-primary btn-block" type="button" onclick='addReqGroup()'>Add Requirement Group</button>
+													<div class="col-lg-10">
+														<button type="submit" class="btn btn-send">Report</button>
+														<button type="button" class="btn btn-send">Cancel</button>
+													</div>
+												</div>
 											</form>
 										</div>
 									</div><!-- /.modal-content -->
 								</div><!-- /.modal-dialog -->
 							</div><!-- /.modal -->							
-							<div class="modal fade" id="removeReqGroup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-								<div class="modal-dialog">
-									<div class="modal-content">
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-											<h4 class="modal-title">Remove Testcase</h4>
-										</div>
-										<div class="modal-body">
-											<form class="form-horizontal" role="form">
-												Are you sure you want to remove Test case?
-												<br><br>
-												<div class="form-group">
-													<div class="col-lg-10">
-														<button type="submit" class="btn btn-send">Remove</button>
-														<button type="submit" class="btn btn-send">Cancel</button>
-													</div>
-												</div>
-											</form>
-										</div>
-									</div><!-- /.modal-content -->
-								</div><!-- /.modal-dialog -->
-							</div><!-- /.modal -->
-							<div class="modal fade" id="addReq" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-								<div class="modal-dialog">
-									<div class="modal-content">
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-											<h4 class="modal-title">Add Requirement</h4>
-										</div>
-										<div class="modal-body">
-											<form role="form" id="individual" >
-												<div class="form-group">
-													<select name="parent" class="form-control"  id="parent_project_id" disabled>
-														<option value="default">Select Project</option>
-														<?php foreach($project_requirements as $project){ ?>
-														<option value="<?= $project['project_id']?>"><?= $project['project_name']?></option>
-														<?php } ?>
-													</select>
-												</div>
-												<div class="form-group">
-													<input name="parentname" class="form-control" type="text"  id="parentname" disabled/>
-													<input name="parent" type="hidden" id="parentid"/>
-												</div> 
-												<div class="form-group">
-													<div class="form-group">
-														<input type="text" class="form-control" name="requirementname" id="req_group_name" placeholder="Requirement Name">
-													</div>
-												</div>
-												<div class="row">
-													<div class="form-group col-md-6">
-														<select name="type" class="form-control"  id="type">
-															<option value="default">Select Type</option>
-															<option value="1">Functional</option>
-															<option value="0">Non-Functional</option>
-														</select>
-													</div>
-													<div class="form-group col-md-6">
-														<select name="priority" class="form-control" id="priority">
-															<option value="default">Select Priority</option>
-															<option value="1">Very High</option>
-															<option value="2">High</option>
-															<option value="3">Medium</option>
-															<option value="4">Low</option>
-															<option value="5">Very Low</option>
-														</select>
-													</div>
-												</div>
-												<div class="form-group">
-													<textarea class="form-control" rows="4" id="requirementdescription" name="requirementdescription" placeholder="Requirement Description"></textarea>
-												</div>
-												<button class="btn btn-lg btn-primary btn-block" onclick="addReq()">Add Requirement</button>
-											</form>
-										</div>
-									</div><!-- /.modal-content -->
-								</div><!-- /.modal-dialog -->
-							</div><!-- /.modal -->							
-							<div class="modal fade" id="removeSprint" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-								<div class="modal-dialog">
-									<div class="modal-content">
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-											<h4 class="modal-title">Remove Requirement</h4>
-										</div>
-										<div class="modal-body">
-											<form class="form-horizontal" role="form">
-												Are you sure you want to remove Requirement?
-												<br><br>
-												<div class="form-group">
-													<div class="col-lg-10">
-														<button type="submit" class="btn btn-send">Remove</button>
-														<button type="submit" class="btn btn-send">Cancel</button>
-													</div>
-												</div>
-											</form>
-										</div>
-									</div><!-- /.modal-content -->
-								</div><!-- /.modal-dialog -->
-							</div><!-- /.modal -->
 						</div>
 						<div id = "jstree_div_container">
 							<div id="jstree_div">
@@ -201,11 +109,15 @@
 										</div>
 										<div class="row">
 											<div class="form-group col-md-6">
-												<select name="parent" class="form-control"  id="runby">
-													<option value="default">Select Runby</option>
+												<select name="parent" class="form-control"  id="testcase_status">
+													<option value="default">Set Status</option>
+													<option value="1">Success</option>
 												</select>
 											</div>	
-										</div>											                      
+											<div class="col-md-6">
+												<a class="btn" data-toggle="modal" href="#reportBug" id="bugreportbtn"><span class="fa fa-minus-square"> Report Bug</a>
+											</div>
+										</div>
 										<button class="btn btn-lg btn-primary btn-block" type="button" onclick='editReqGroup()'>Edit TestCase</button>
 									</div>
 								</form>
@@ -239,6 +151,17 @@ function jstreeEventHandler(evt,data){
 
 	proj_id = res[1];
 	node_name = data.node["id"];
+
+	$("#bugreportbtn").hide();
+	$('#testcase_status').on('change', function() {
+	  console.log(this.value);
+		if(this.value == "1"){
+			$("#bugreportbtn").hide();
+		}
+		else{
+			$("#bugreportbtn").show();
+		}
+	});
 
 	if(res[0]=='reqGroup'){
 		$('#message').hide();
